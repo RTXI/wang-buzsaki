7f33e40127368a5234642759a3eae6d1b821ad19
anselg <aesthapus@gmail.com>
2015-03-19 20:45:22 -0400

    removed legacy files

--- wang-buzsaki.cpp
+++ wang-buzsaki.cpp
@@ -128,6 +128,7 @@ wbscaled::wbscaled(void) : DefaultGUIModel("Wang Buzsaki - scaled", ::vars, ::nu
 	createGUI(vars, num_vars);
 	update(INIT);
 	refresh();
+	QTimer::singleShot(0, this, SLOT(resizeMe()));
 }
 
 wbscaled::~wbscaled(void)
